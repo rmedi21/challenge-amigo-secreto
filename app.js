02f7933cc26d432f7f0113a466c039a5c683e835
rmedi21 <rmedina@mba.pad.edu>
2025-02-06 21:51:49 -0500

    Actualiza el index

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+function agregarAmigo() {
+    alert("Amigo agregado");
+}
