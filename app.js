4babd7d8ba018bb01a01764cabf0c78b40ac79f2
rmedi21 <rmedina@mba.pad.edu>
2025-02-07 19:24:13 -0500

    Actualiza app

--- app.js
+++ app.js
@@ -1,4 +1,27 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+document.getElementById("amigo").focus();
+
+
 function agregarAmigo() {
-    alert("Amigo agregado");
+    let txtAmigo = document.getElementById("amigo");
+    let amigo = txtAmigo.value;
+
+    if (amigo == "") {
+        alert("Ingrese un nombre por favor.");
+    } else {
+        amigos.push(amigo);
+        txtAmigo.value = "";
+        txtAmigo.focus();
+    }
+}
+
+function sortearAmigo() {
+    mostrarAmigos();
+}
+
+function mostrarAmigos() {
+    for (const amigo of amigos) {
+        console.log(amigo);
+    }
 }
