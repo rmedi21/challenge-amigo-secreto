c706ac34d0b2c5cfd8bc53e8e6758967567e5293
rmedi21 <rmedina@mba.pad.edu>
2025-02-08 01:18:52 -0500

    Actualiza app v1.0

--- app.js
+++ app.js
@@ -11,17 +11,36 @@ function agregarAmigo() {
         alert("Ingrese un nombre por favor.");
     } else {
         amigos.push(amigo);
+        document.getElementById("listaAmigos").innerHTML += `<li>${amigo}</li>`
         txtAmigo.value = "";
         txtAmigo.focus();
     }
 }
 
 function sortearAmigo() {
-    mostrarAmigos();
+    if (amigos.length == 0) {
+        alert("No hay amigos para sortear.")
+    }
+    else {
+        const indAleatorio = obtenerNumeroAleatorio(0, amigos.length - 1);
+        document.getElementById("resultado").innerHTML = `<li>${amigos[indAleatorio]}</li>`
+    }
+}
+
+function obtenerNumeroAleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function mostrarAmigos() {
     for (const amigo of amigos) {
         console.log(amigo);
     }
+}
+
+function limpiarLista() {
+    amigos = [];
+    document.getElementById("listaAmigos").innerHTML = "";
+    document.getElementById("resultado").innerHTML = "";
+    txtAmigo.value = "";
+    txtAmigo.focus();
 }
